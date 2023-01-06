e736d1b9bbdede5d5a7d51cc393fce8176300744
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2023-01-05 18:18:21 -0600

    clicking buttons does stuff

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,37 +1,128 @@
-module Pages.Home_ exposing (page)
+module Pages.Home_ exposing (Model, Msg, page)
 
 import Html exposing (..)
 import Html.Attributes as Attr exposing (class, classList, max, value)
+import Html.Events
+import Page exposing (Page)
 import View exposing (View)
 
 
-page : View msg
+
+-- PAGE
+
+
+page : Page Model Msg
 page =
+    Page.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    { enemyHealth : Int
+    , playerHealth : Int
+    }
+
+
+init : Model
+init =
+    { enemyHealth = maxHealth
+    , playerHealth = maxHealth
+    }
+
+
+maxHealth : Int
+maxHealth =
+    100
+
+
+
+-- UPDATE
+
+
+type Msg
+    = UserClickedFight
+    | UserClickedRun
+    | UserClickedPlayAgain
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UserClickedFight ->
+            { model
+                | enemyHealth =
+                    Basics.max
+                        0
+                        (model.enemyHealth - 10)
+            }
+
+        UserClickedRun ->
+            { model
+                | playerHealth =
+                    Basics.max
+                        0
+                        (model.playerHealth - 10)
+            }
+
+        UserClickedPlayAgain ->
+            { model
+                | enemyHealth = maxHealth
+                , playerHealth = maxHealth
+            }
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
     { title = "Jokemon"
     , body =
         [ div [ class "game border-regular" ]
-            [ viewFightScene
-            , viewControls
-            ]
+            (if model.enemyHealth <= 0 then
+                [ span [ class "message" ]
+                    [ text "You won!" ]
+                , viewPlayAgainButton
+                ]
+
+             else if model.playerHealth <= 0 then
+                [ span [ class "message" ]
+                    [ text "You lost!" ]
+                , viewPlayAgainButton
+                ]
+
+             else
+                [ viewFightScene model
+                , viewControls
+                ]
+            )
         ]
     }
 
 
-viewFightScene : Html msg
-viewFightScene =
+viewFightScene : Model -> Html msg
+viewFightScene model =
     div [ class "col pad-md gap-md" ]
-        [ viewEnemyRow
-        , viewPlayerRow
+        [ viewEnemyRow model.enemyHealth
+        , viewPlayerRow model.playerHealth
         ]
 
 
-viewEnemyRow : Html msg
-viewEnemyRow =
+viewEnemyRow : Int -> Html msg
+viewEnemyRow enemyHealth =
     div [ class "row center-h gap-md pad-x-md" ]
         [ viewPokemonStatus
             { name = "Doge"
             , level = 5
-            , health = ( 50, 100 )
+            , health = ( enemyHealth, maxHealth )
             , border = BottomLeft
             }
         , viewSprite "🐕"
@@ -91,29 +182,43 @@ viewHealthbar ( value, max ) =
         ]
 
 
-viewPlayerRow : Html msg
-viewPlayerRow =
+viewPlayerRow : Int -> Html msg
+viewPlayerRow playerHealth =
     div [ class "row center-h gap-md pad-x-md" ]
         [ viewSprite "🐈"
         , viewPokemonStatus
             { name = "Kitty"
             , level = 5
-            , health = ( 10, 100 )
+            , health = ( playerHealth, maxHealth )
             , border = BottomRight
             }
         ]
 
 
-viewControls : Html msg
+viewControls : Html Msg
 viewControls =
     div [ class "row border-top-regular" ]
         [ span [ class "fill" ] []
         , span [ class "fill pad-sm" ]
             [ div [ class "grid border-regular pad-md" ]
-                [ span [] [ text "Fight" ]
-                , span [] [ text "PKMN" ]
-                , span [] [ text "Item" ]
-                , span [] [ text "Run" ]
+                [ button
+                    [ Html.Events.onClick UserClickedFight
+                    ]
+                    [ text "Fight" ]
+                , button [] [ text "PKMN" ]
+                , button [] [ text "Item" ]
+                , button
+                    [ Html.Events.onClick UserClickedRun
+                    ]
+                    [ text "Run" ]
                 ]
             ]
         ]
+
+
+viewPlayAgainButton : Html Msg
+viewPlayAgainButton =
+    button
+        [ Html.Events.onClick UserClickedPlayAgain
+        ]
+        [ text "Play again" ]
